51e62ece10fc3bee49f002d23d37b8d1846a7bfb
DarksideJP <darknebula1419@gmail.com>
2020-02-02 10:11:43 +0000

    [Finish]4-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_02_092023) do
+ActiveRecord::Schema.define(version: 2020_02_02_093008) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
